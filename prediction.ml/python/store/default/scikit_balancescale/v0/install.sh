da6508db89c16c5ccb8b3028fcb86908c40e2847
cfregly <chris@fregly.com>
2017-04-16 13:42:51 -0400

    sourcing

--- prediction.ml/python/store/default/scikit_balancescale/v0/install.sh
+++ prediction.ml/python/store/default/scikit_balancescale/v0/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # ensure we're in the root conda environment
-source activate root
+#source activate root
 
 # Commenting this out for now
 # remove a possibly pre-existing `model_environment` environment.
 #conda env remove -n model_environment --yes || true
 
 # create a new, empty `model_environment` environment.
-conda create --yes -n model_environment python=3.5
+#conda create --yes -n model_environment python=3.5
 
 # Activate it.
 source activate model_environment
