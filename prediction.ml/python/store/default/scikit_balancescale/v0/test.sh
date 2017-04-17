2444b2c646284ecbc88e2c990c8c11a3c765854e
cfregly <chris@fregly.com>
2017-04-17 20:56:56 +0000

    updated comments

--- prediction.ml/python/store/default/scikit_balancescale/v0/test.sh
+++ prediction.ml/python/store/default/scikit_balancescale/v0/test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Testing model..."
 export PIO_MODEL_FILENAME=$(ls *.pkl | sed -n 1p)
 
 [ -s ./test_predict.py ] && python test_predict.py \
                                 $PIO_MODEL_FILENAME \
                                 "./test_inputs.txt" \
                                 "./test_outputs.txt"
+echo "...Done!"
